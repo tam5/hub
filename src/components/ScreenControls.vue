899ccceaebd562ce260a78c95296a93fc42da13d
tam5 <arimiller92@gmail.com>
2020-07-23 09:46:22 -0400

    start

---
+++ src/components/ScreenControls.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h2>Screens</h2>
+    <ScreensOption left="work" right="work" />
+    <ScreensOption left="personal" right="personal" />
+    <ScreensOption left="work" right="personal" />
+    <ScreensOption left="personal" right="work" />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import ScreensOption from "./ScreensOption.vue";
+
+export default Vue.extend({
+  name: "ScreenControls",
+  components: {
+    ScreensOption
+  }
+});
+</script>
+
+<style scoped lang="scss"></style>
---
+++ src/components/ScreensOption.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    hi
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "ScreensOption",
+  props: ["left", "right"]
+});
+</script>
+
+<style scoped lang="scss"></style>
